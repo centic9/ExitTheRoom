906bb96400bf1238ff1a74aad8de715d68ebb1b4
preis <preis@55953d02-9106-0410-9835-bf18e61e9b54>
2020-10-10 19:42:03 +0000

    Add simple remote-control via a control-file

--- ExitTheRoom.py
+++ ExitTheRoom.py
@@ -2,11 +2,32 @@
 
 # import RPi.GPIO as GPIO
 # import TM1638
+import os
 from time import sleep
 
 
+CONTROL_FILE = "/tmp/control.txt"
+
+
+def read_file():
+    # print("Reading file")
+    try:
+        file = open(CONTROL_FILE, "r")
+        msg = file.read()
+        print("Had control-command: " + msg)
+        file.close()
+
+        os.remove(CONTROL_FILE)
+    except FileNotFoundError:
+        pass
+
+
+print("Starting main loop")
 while True:
-    print("Running main loop")
+    # print("Running main loop")
+
+    # check control
+    read_file()
 
     # read inputs
 
