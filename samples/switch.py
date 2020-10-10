977089880213e98b747bb9181f3cf4b44870d5e6
preis <preis@55953d02-9106-0410-9835-bf18e61e9b54>
2020-10-10 19:14:37 +0000

    reformat samples

--- samples/switch.py
+++ samples/switch.py
@@ -1,13 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
+
 def button_callback(channel):
     print("Button was pushed!")
+
 
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+# Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     if GPIO.input(19) == GPIO.HIGH:
--- samples/switch2.py
+++ samples/switch2.py
@@ -1,22 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
+
 def button_callback(channel):
-  print("Button was pushed!")
+    print("Button was pushed!")
+
 
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 
 for i in range(1, 30):
-  GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+    # Set pin to be an input pin and set initial value to be pulled low (off)
+    GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
-  for i in range(1, 30):
-    if GPIO.input(i) == GPIO.HIGH:
-        print("Button " + str(i) + " was pushed!")
-    else:
-        print("Button " + str(i) + " was not pushed")
-  time.sleep(0.5)
+    for i in range(1, 30):
+        if GPIO.input(i) == GPIO.HIGH:
+            print("Button " + str(i) + " was pushed!")
+        else:
+            print("Button " + str(i) + " was not pushed")
+    time.sleep(0.5)
 
-GPIO.cleanup() # Clean up
-
+# Clean up
+GPIO.cleanup()
