7b5a309207b081db472b1add7a5b48a7cbf26eda
preis <preis@55953d02-9106-0410-9835-bf18e61e9b54>
2020-10-10 19:15:01 +0000

    Initial main application

---
+++ ExitTheRoom.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# import RPi.GPIO as GPIO
+# import TM1638
+from time import sleep
+
+
+while True:
+    print("Running main loop")
+
+    # read inputs
+
+    # check mappings
+
+    # perform outputs
+
+    # remember achievements
+
+    # delay
+    sleep(0.1)
