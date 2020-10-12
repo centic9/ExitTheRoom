6d5d73c02993e78b94b64d9f25e647aae5ff2f45
preis <preis@55953d02-9106-0410-9835-bf18e61e9b54>
2020-10-12 05:39:39 +0000

    Remove unused imports

--- src/test/java/org/dstadler/exit/TM1638Test.java
+++ src/test/java/org/dstadler/exit/TM1638Test.java
@@ -6,8 +6,6 @@ import com.pi4j.io.gpio.RaspiPin;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
