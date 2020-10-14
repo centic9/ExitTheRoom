9d97ceb47ab069b10328677421a52be42a8c6de9
preis <preis@55953d02-9106-0410-9835-bf18e61e9b54>
2020-10-14 19:50:03 +0000

    Adjust pins for LED

--- samples/led.py
+++ samples/led.py
@@ -4,15 +4,17 @@ import RPi.GPIO as GPIO
 import time
 
 
+pin = 21
+
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.OUT)
+GPIO.setup(pin, GPIO.OUT)
 
 for i in range(5):
-    GPIO.output(4, GPIO.HIGH)
+    GPIO.output(pin, GPIO.HIGH)
     time.sleep(0.5)
-    GPIO.output(4, GPIO.LOW)
+    GPIO.output(pin, GPIO.LOW)
     time.sleep(0.5)
 
 GPIO.cleanup() # Clean up
