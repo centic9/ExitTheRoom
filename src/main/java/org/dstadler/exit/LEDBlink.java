12e32a3581d2f78783c0463ad0f3d0532bd54af6
preis <preis@55953d02-9106-0410-9835-bf18e61e9b54>
2020-10-22 07:37:08 +0000

    Add a LED-Blinking sample

---
+++ src/main/java/org/dstadler/exit/LEDBlink.java
@@ -0,0 +1,41 @@
+package org.dstadler.exit;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.RaspiPin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LEDBlink {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Setting up GPIO LED Blinking");
+
+        // create gpio controller instance
+        final GpioController gpio = GpioFactory.getInstance();
+
+        List<GpioPinDigitalOutput> leds = new ArrayList<>();
+        for (Pin pin : RaspiPin.allPins(PinMode.DIGITAL_OUTPUT)) {
+            leds.add(gpio.provisionDigitalOutputPin(pin, "Pin-" + pin.getAddress()));
+        }
+
+        System.out.println("Setup finished, waiting for CTRL-C");
+        // wait for CTRL-C
+        int i = 0;
+        int led = 0;
+        while (true) {
+            if (i % 10 == 0) {
+                System.out.println("LED: " + led + ": " + (i % 20 == 0));
+                leds.get(led).setState(i % 20 == 0);
+                led = (led + 1) % leds.size();
+            }
+
+            i++;
+
+            Thread.sleep(100);
+        }
+    }
+}
